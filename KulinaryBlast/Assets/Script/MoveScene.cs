737dfe4a665dd222f652cf2105346f42b5631bbc
Muhammad Figo Mahendra <83908172+figs24@users.noreply.github.com>
2021-11-26 15:35:03 +0800

    Scene
    membuat Scene game Kulinary Blast yang terdiri dari scene Main menu,Level,Setting,Game

---
+++ KulinaryBlast/Assets/Script/MoveScene.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void onPlay()
+    {
+        SceneManager.LoadScene("Level");
+    }
+    public void onSet()
+    {
+        SceneManager.LoadScene("Setting");
+    }
+    public void onPlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void onMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void onLose()
+    {
+        SceneManager.LoadScene("Lose");
+    }
+}
