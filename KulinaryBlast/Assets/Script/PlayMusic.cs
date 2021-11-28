92c08a35352a863166b77c98f756bbb99326a5a4
Muhammad Figo Mahendra <83908172+figs24@users.noreply.github.com>
2021-11-28 23:50:28 +0800

    settings
    Menambah tombol untuk lagu dan memasukkan asset music

---
+++ KulinaryBlast/Assets/Script/PlayMusic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusic : MonoBehaviour
+{
+    public AudioSource lagu1, lagu2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        if (gameObject.name == "lag1")
+        {
+            lagu1.Play();
+            lagu2.Stop();
+        }
+        else
+        {
+            lagu1.Stop();
+            lagu2.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
