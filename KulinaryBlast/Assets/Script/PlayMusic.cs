ba1a61ee1b766aeeb74f34b06dc828d691ed1851
Muhammad Figo Mahendra <83908172+figs24@users.noreply.github.com>
2021-11-29 07:59:34 +0800

    update in settings
    memperbaiki kesalahan sebelumnya dan membuat lagu tetap berjalan discene lainnya

--- KulinaryBlast/Assets/Script/PlayMusic.cs
+++ KulinaryBlast/Assets/Script/PlayMusic.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayMusic : MonoBehaviour
 {
     public AudioSource lagu1, lagu2;
+    public Slider volumeMusic;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,15 +14,17 @@ public class PlayMusic : MonoBehaviour
     }
     void OnMouseDown()
     {
-        if (gameObject.name == "lag1")
+        if (gameObject.name == "Tombol lagu 1")
         {
             lagu1.Play();
             lagu2.Stop();
+            lagu1.volume = volumeMusic.value;
         }
         else
         {
             lagu1.Stop();
             lagu2.Play();
+            lagu2.volume = volumeMusic.value;
         }
     }
 
