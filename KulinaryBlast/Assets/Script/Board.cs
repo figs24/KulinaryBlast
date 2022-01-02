665aee7e555136ca154b7af11b7f3e04fd62c289
Muhammad Figo Mahendra <83908172+figs24@users.noreply.github.com>
2022-01-02 22:24:43 +0800

    update
    update ke 3 game kulinary blast

--- KulinaryBlast/Assets/Script/Board.cs
+++ KulinaryBlast/Assets/Script/Board.cs
@@ -2,19 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameState
+{
+    wait,
+    move
+}
 public class Board : MonoBehaviour
 {
-
+    public GameState currentState = GameState.move;
     public int width;
     public int height;
+    public int offSet;
     private BackgroundTile[,] allTiles;
     public GameObject tilePrefab;
     public GameObject[] dots;
     public GameObject[,] allDots;
+    private FindMatches findMatches;
 
     // Start is called before the first frame update
     void Start()
     {
+        findMatches= FindObjectOfType<FindMatches>();
         allTiles = new BackgroundTile[width, height];
         allDots = new GameObject[width, height];
         SetUp();
@@ -27,7 +35,7 @@ public class Board : MonoBehaviour
         {
             for (int j = 0; j < height; j++)
             {
-                Vector3 tempPosition = new Vector3(i, j);
+                Vector3 tempPosition = new Vector3(i, j+offSet);
                 GameObject backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity) as GameObject;
                 backgroundTile.transform.parent = this.transform;
                 backgroundTile.name = "( " + i + ", " + j + " )";
@@ -40,6 +48,8 @@ public class Board : MonoBehaviour
                 }
                 maxIterations = 0;
                 GameObject dot = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
+                dot.GetComponent<Dot>().row = j;
+                dot.GetComponent<Dot>().column = i;
                 dot.transform.parent = this.transform;
                 dot.name = "(" + i + "," + j + ")";
                 allDots[i, j] = dot;
@@ -83,6 +93,7 @@ public class Board : MonoBehaviour
     {
         if (allDots[column, row].GetComponent<Dot>().isMatched)
         {
+            findMatches.currentMatches.Remove(allDots[column, row]);
             Destroy(allDots[column, row]);
             allDots[column, row] = null;
         }
@@ -130,10 +141,12 @@ public class Board : MonoBehaviour
             {
                 if (allDots[i, j] == null)
                 {
-                    Vector3 tempPosition = new Vector3(i, j);
+                    Vector3 tempPosition = new Vector3(i, j+offSet);
                     int dotToUse = Random.Range(0, dots.Length);
                     GameObject piece = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
                     allDots[i, j] = piece;
+                    piece.GetComponent<Dot>().row = j;
+                    piece.GetComponent<Dot>().column = i;
                 }
             }
         }
@@ -165,5 +178,7 @@ public class Board : MonoBehaviour
             yield return new WaitForSeconds(.5f);
             DestroyMatches();
         }
+        yield return new WaitForSeconds(.5f);
+        currentState = GameState.move;
     }
 }
